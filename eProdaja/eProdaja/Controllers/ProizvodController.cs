e59acd4ce293879a4a9c29c2129a61e20721c33a
Niaina Sadiković-Ratsimanohatra <niaina.s.r@edu.fit.ba>
2021-03-26 01:14:10 +0100

    V1 -Git repo, basic REST API, Dependency injection

---
+++ eProdaja/eProdaja/Controllers/ProizvodController.cs
@@ -0,0 +1,50 @@
+﻿using eProdaja.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProizvodController : ControllerBase
+    {
+        public IProizvodService _proizvodService { get; set; }
+        public ProizvodController(IProizvodService proizvodService)
+        {
+            _proizvodService = proizvodService;
+        }
+       
+        [HttpGet]
+        public IEnumerable<Proizvod> Get()
+        {
+            return _proizvodService.Get();
+        }
+
+        [HttpGet("{id}")]
+        public Proizvod GetById(int id)
+        {
+            return _proizvodService.GetById(id);
+        }
+        [HttpPost]
+        public Proizvod Insert(Proizvod proizvod)
+        {
+            return _proizvodService.Insert(proizvod);
+            
+        }
+        [HttpPut("{id}")]
+        public Proizvod Update(int id, Proizvod proizvod)
+        {
+            return _proizvodService.Update(id, proizvod);
+        }
+
+    }
+    public class Proizvod
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+    }
+}
---
+++ eProdaja/eProdaja/Services/ProizvodService.cs
@@ -0,0 +1,51 @@
+﻿using eProdaja.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodService :IProizvodService
+    {
+        static List<Proizvod> _proizvodi;
+        static ProizvodService()
+        {
+            _proizvodi = new List<Proizvod>()
+                {
+
+                    new Proizvod
+                    {
+                        Id=1,
+                        Naziv="Laptop"
+                    },
+                    new Proizvod
+                    {
+                        Id=2,
+                        Naziv="Miš"
+                    }
+                };
+        }
+
+        public IEnumerable<Proizvod> Get()
+        {
+            return _proizvodi;
+        }
+
+        public Proizvod GetById(int id)
+        {
+            return _proizvodi.FirstOrDefault(p => p.Id == id);
+        }
+        public Proizvod Insert(Proizvod proizvod)
+        {
+            _proizvodi.Add(proizvod);
+            return proizvod;
+        }
+        public Proizvod Update(int id, Proizvod proizvod)
+        {
+            var current = _proizvodi.FirstOrDefault(p => p.Id == id);
+            current.Naziv = proizvod.Naziv;
+            return current;
+        }
+    }
+}
